7c79ff7bfec678216f75c75b1e8bb2cadd5a600c
Alexander Tkachuk <tkachuka2020@gmail.com>
2021-07-10 14:58:09 +0300

    Реализован функционал, требуемый в ТЗ

--- src/app.js
+++ src/app.js
@@ -63,3 +63,120 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+const refs = {
+  gallery: document.querySelector('.js-gallery'),
+  modalBox: document.querySelector('.js-lightbox'),
+  modalBoxBackdrop: document.querySelector('.lightbox__overlay'),
+  modalBoxImage: document.querySelector('.lightbox__image'),
+  modalBoxCloseBtn: document.querySelector('.lightbox__button'),
+};
+
+function createGalleryMarkup(images) {
+  return images
+    .map(({ original, preview, description }) => {
+      return `<li class="gallery__item">
+    <a
+      class="gallery__link"
+      href="${original}"
+    >
+      <img
+        loading="lazy"
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+              </li>`
+    })
+    .join('');
+};
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+refs.galleryItems = document.querySelectorAll('.gallery__image');
+
+
+
+refs.gallery.addEventListener('click', onGalleryItemClick);
+refs.modalBoxBackdrop.addEventListener('click', onBackdropClick);
+refs.modalBoxCloseBtn.addEventListener('click', onModalBoxClose);
+window.addEventListener('keydown', onEscapeKeyPress);
+
+
+
+function onGalleryItemClick(event) {
+
+  event.preventDefault();
+  
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  };
+  
+  onModalBoxOpen();
+  setModalBoxImageSource(event.target.dataset.source);
+  setModalBoxImageDescription(event.target.alt);
+  
+  const isModalBoxOpen = refs.modalBox.classList.contains('is-open');
+
+  if (isModalBoxOpen) {
+    let currentIndex = getCurrentModalBoxImageIndex(event.target);
+
+    window.addEventListener('keydown', (event) => {
+      
+      if (event.code === 'ArrowRight') {
+        if (currentIndex === refs.galleryItems.length - 1) {
+          return;
+        }
+        currentIndex += 1;
+        refs.modalBoxImage.src = refs.galleryItems[currentIndex].dataset.source;
+      }
+      if (event.code === 'ArrowLeft') {
+        if (currentIndex === 0) {
+          return;
+        }
+        currentIndex -= 1;
+        refs.modalBoxImage.src = refs.galleryItems[currentIndex].dataset.source;
+      }
+    });
+    
+  } 
+};
+
+function onModalBoxOpen() {
+  refs.modalBox.classList.add('is-open');
+};
+
+function onModalBoxClose() {
+  refs.modalBox.classList.remove('is-open');
+  refs.modalBoxImage.src = '';
+  refs.modalBoxImage.alt = '';
+};
+
+
+function onEscapeKeyPress(event) {
+  if (event.code === 'Escape') {
+    onModalBoxClose();
+  }
+};
+
+function onBackdropClick(event) {
+  if (event.target === event.currentTarget) {
+    onModalBoxClose();
+  }
+};
+
+function setModalBoxImageSource(source) {
+  refs.modalBoxImage.src = source;
+};
+
+function setModalBoxImageDescription(description) {
+  refs.modalBoxImage.alt = description;
+};
+
+function getCurrentModalBoxImageIndex(image) {
+  let modalBoxImageIndex = [].indexOf.call(refs.galleryItems, image);
+  return modalBoxImageIndex;
+};
+
